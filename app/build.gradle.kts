5e0ad55d9e28989008ec6981780ac73979acaba6
Julia Kwiatkowska <268375@student.pwr.edu.pl>
2024-04-22 18:03:57 +0200

    start connect cloud firestore with our project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
