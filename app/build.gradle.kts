b5ff67e510c0f48d824317c1d461bae022af74a9
Oliwia Zielińska <268480@student.pwr.edu.pl>
2024-10-03 12:40:09 +0200

    Adds MPAndroidChart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -56,4 +55,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation ("androidx.work:work-runtime-ktx:2.7.1")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
