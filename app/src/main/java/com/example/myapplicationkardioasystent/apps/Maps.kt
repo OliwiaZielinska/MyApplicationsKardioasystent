f9e900ab381fa1fe3d14c8039c863beb19032999
Oliwia Zielińska <268480@student.pwr.edu.pl>
2024-11-13 10:04:47 +0100

    Small improvements with Maps and navigation

--- app/src/main/java/com/example/myapplicationkardioasystent/apps/Maps.kt
+++ app/src/main/java/com/example/myapplicationkardioasystent/apps/Maps.kt
@@ -36,17 +36,19 @@ import org.json.JSONObject
  */
 class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
-    private lateinit var mMap: GoogleMap //obiekt mapy
-    private lateinit var fusedLocationClient: FusedLocationProviderClient //klient do uzyskiwania lokalizacji
+    private lateinit var mMap: GoogleMap    //obiekt map
+    private lateinit var fusedLocationClient: FusedLocationProviderClient   //klient do uzyskiwania lokalizacji
     private lateinit var lastLocation: Location //ostatnia znaleziona lokalizacja użytkownika
 
-
-    private lateinit var returnFromMapsButton: Button //przycisk do powrotu do głównej aktywności
+    private lateinit var returnFromMapsButton: Button   //przycisk do powrotu do głównej aktywności
+    private lateinit var navigationButton: Button   //przycisk do przejścia do map Google i nawigacji
     private lateinit var searchButton: Button //przycisk wyszukiwania
     private lateinit var typeSpinner: Spinner //spinner wyboru typu miejsca (lekarz/apteka/szpital)
     private lateinit var distanceSeekBar: SeekBar
     private lateinit var distanceTextView: TextView
     private var searchRadius = 5000 //początkowy promień wyszukiwania (5 km)
+
+    private var destinationLatLng: LatLng? = null // Przechowywanie wybranego miejsca
 
     companion object {
         private const val LOCATION_REQUEST_CODE = 1 //kod żądania uprawnień do lokalizacji
@@ -60,28 +62,33 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.maps)
-        //pobranie ID użytkownika z przesłanych danych
-        val uID = intent.getStringExtra("uID")
+
         //inicjalizacja fragmentu mapy
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         //inicjalizacja klienta do lokalizacji
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         //inicjalizacja elementów UI
         returnFromMapsButton = findViewById(R.id.returnFromMapsButton)
-        searchButton = findViewById(R.id.searchButton) // Inicjalizacja przycisku
-        typeSpinner = findViewById(R.id.typeSpinner) // Inicjalizacja spinnera
+        searchButton = findViewById(R.id.searchButton)  //Inicjalizacja przycisku
+        typeSpinner = findViewById(R.id.typeSpinner)    //Inicjalizacja spinnera
+        navigationButton = findViewById(R.id.navigationButton)
 
         // Inicjalizacja spinnera
         val types = arrayOf("Apteka", "Lekarz", "Szpital")
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, types)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        typeSpinner.adapter = adapter
-        //inicjalizacja spinnera z typami miejsc
+        typeSpinner.adapter = adapter   //inicjalizacja spinnera z typami miejsc
         val userId = FirebaseAuth.getInstance().currentUser!!.email
         returnFromMapsButton.setOnClickListener {
             openMainActivity(userId.toString()) //powrót do głównej aktywności
         }
+
+        navigationButton.setOnClickListener {
+            openGoogleMapsForNavigation()
+        }
+
         //inicjalizacja suwaka i TextView do wyświetlania wybranej odległości
         distanceSeekBar = findViewById(R.id.distanceSeekBar)
         distanceTextView = findViewById(R.id.distanceTextView)
@@ -93,28 +100,25 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
         //obsługa zmiany wartości suwaka
         distanceSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                searchRadius = distanceValues[progress] // Zaktualizowanie promienia wyszukiwania
+                searchRadius = distanceValues[progress]
                 distanceTextView.text = "Odległość: ${distanceLabels[progress]}"
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
         //ustawienie listenera dla przycisku wyszukiwania
         searchButton.setOnClickListener {
             val selectedType = typeSpinner.selectedItem.toString()
             when (selectedType) {
-                "Apteka" -> findNearbyPlaces("pharmacy") //wyszukiwanie aptek
-                "Lekarz" -> findNearbyPlaces("doctor") //wyszukiwanie lekarzy
-                "Szpital" -> findNearbyPlaces("hospital") //wyszukiwanie szpitali
+                "Apteka" -> findNearbyPlaces("pharmacy")    //wyszukowanie aptek
+                "Lekarz" -> findNearbyPlaces("doctor")  //wyszukiwanie lekarzy
+                "Szpital" -> findNearbyPlaces("hospital")   //wyszukiwanie szpitali
                 else -> Toast.makeText(this, "Wybierz typ miejsca", Toast.LENGTH_SHORT).show()
             }
         }
-
     }
+
     /**
      * Metoda do wyszukiwania pobliskich miejsc według podanego typu.
      *
@@ -126,42 +130,38 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
             val locationString = "${lastLocation.latitude},${lastLocation.longitude}"
             val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$locationString&radius=$searchRadius&type=$type&key=$apiKey"
 
-
             // Żądanie HTTP do Google Places API
-            val request = object : StringRequest(
-                Method.GET, url,
-                Response.Listener { response ->
-                    try {
-                        // Parsowanie odpowiedzi JSON
-                        val jsonObject = JSONObject(response)
-                        val results = jsonObject.getJSONArray("results")
+            val request = object : StringRequest(Method.GET, url, Response.Listener { response ->
+                try {
+                    // Parsowanie odpowiedzi JSON
+                    val jsonObject = JSONObject(response)
+                    val results = jsonObject.getJSONArray("results")
 
-                        // Usunięcie istniejących markerów
-                        mMap.clear()
+                    // Usunięcie istniejących markerów
+                    mMap.clear()
 
-                        // Iteracja po wynikach i dodanie markerów na mapie
-                        for (i in 0 until results.length()) {
-                            val place = results.getJSONObject(i)
-                            val latLng = place.getJSONObject("geometry").getJSONObject("location")
-                            val lat = latLng.getDouble("lat")
-                            val lng = latLng.getDouble("lng")
-                            val placeName = place.getString("name")
+                    // Iteracja po wynikach i dodanie markerów na mapie
+                    for (i in 0 until results.length()) {
+                        val place = results.getJSONObject(i)
+                        val latLng = place.getJSONObject("geometry").getJSONObject("location")
+                        val lat = latLng.getDouble("lat")
+                        val lng = latLng.getDouble("lng")
+                        val placeName = place.getString("name")
 
-                            // Dodanie markera na mapie dla każdego znalezionego miejsca
-                            mMap.addMarker(
-                                MarkerOptions()
-                                    .position(LatLng(lat, lng))
-                                    .title(placeName) // nazwa miejsca jako tytuł markera
-                            )
-                        }
-                    } catch (e: JSONException) {
-                        e.printStackTrace() // obsługa błędów parsowania JSON
+                        // Dodanie markera na mapie dla każdego znalezionego miejsca
+                        mMap.addMarker(
+                            MarkerOptions()
+                                .position(LatLng(lat, lng))
+                                .title(placeName)   //nazwa miejsca jako tytuł markera
+                        )
                     }
-                },
-                Response.ErrorListener { error ->
-                    // obsługa błędów związanych z żądaniem HTTP
-                    Log.e("MapsActivity", "Błąd podczas wyszukiwania: ${error.message}")
-                }) {}
+                } catch (e: JSONException) {
+                    e.printStackTrace() // obsługa błędów parsowania JSON
+                }
+            }, Response.ErrorListener { error ->
+                // obsługa błędów związanych z żądaniem HTTP
+                Log.e("MapsActivity", "Błąd podczas wyszukiwania: ${error.message}")
+            }) {}
 
             // Dodanie żądania do kolejki sieciowej Volley
             Volley.newRequestQueue(this).add(request)
@@ -175,14 +175,14 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
      * @return Klucz API, lub null, jeśli nie został znaleziony.
      */
     private fun getApiKeyFromManifest(): String? {
-        try {
+        return try {
             val applicationInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
             val bundle = applicationInfo.metaData
-            return bundle.getString("com.google.android.geo.API_KEY")
+            bundle.getString("com.google.android.geo.API_KEY")
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
+            null
         }
-        return null
     }
 
     /**
@@ -216,7 +216,6 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
             if (location != null) {
                 lastLocation = location
                 val currentLatLong = LatLng(location.latitude, location.longitude)
-
                 // dodanie markera dla bieżącej lokalizacji użytkownika
                 placeMarkerOnMap(currentLatLong)
 
@@ -242,40 +241,38 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
             val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$locationString&radius=5000&type=$type&key=$apiKey"
 
             // Żądanie HTTP do Google Places API
-            val request = object : StringRequest(
-                Method.GET, url,
-                Response.Listener { response ->
-                    try {
-                        // Parsowanie odpowiedzi JSON
-                        val jsonObject = JSONObject(response)
-                        val results = jsonObject.getJSONArray("results")
+            val request = object : StringRequest(Method.GET, url, Response.Listener { response ->
+                try {
+                    // Parsowanie odpowiedzi JSON
+                    val jsonObject = JSONObject(response)
+                    val results = jsonObject.getJSONArray("results")
 
-                        // Sprawdzenie, czy są jakieś wyniki
-                        if (results.length() > 0) {
-                            val place = results.getJSONObject(0) // pobranie najbliższego miejsca
-                            val latLng = place.getJSONObject("geometry").getJSONObject("location")
-                            val lat = latLng.getDouble("lat")
-                            val lng = latLng.getDouble("lng")
+                    // Sprawdzenie, czy są jakieś wyniki
+                    if (results.length() > 0) {
+                        val place = results.getJSONObject(0)    // pobranie najbliższego miejsca
+                        val latLng = place.getJSONObject("geometry").getJSONObject("location")
+                        val lat = latLng.getDouble("lat")
+                        val lng = latLng.getDouble("lng")
 
-                            // Ustawianie tytułu markera w zależności od typu miejsca
-                            val markerTitle = when (type) {
-                                "pharmacy" -> "Najbliższa apteka"
-                                "doctor" -> "Najbliższy lekarz"
-                                "hospital" -> "Najbliższy szpital"
-                                else -> "Najbliższe miejsce"
-                            }
-
-                            // Dodanie markera na mapie dla najbliższego miejsca
-                            mMap.addMarker(
-                                MarkerOptions()
-                                    .position(LatLng(lat, lng))
-                                    .title(markerTitle) // ustawienie odpowiedniego tytułu markera
-                            )
+                        // Ustawianie tytułu markera w zależności od typu miejsca
+                        val markerTitle = when (type) {
+                            "pharmacy" -> "Najbliższa apteka"
+                            "doctor" -> "Najbliższy lekarz"
+                            "hospital" -> "Najbliższy szpital"
+                            else -> "Najbliższe miejsce"
                         }
-                    } catch (e: JSONException) {
-                        e.printStackTrace() // obsługa błędów parsowania JSON
+
+                        // Dodanie markera na mapie dla najbliższego miejsca
+                        mMap.addMarker(
+                            MarkerOptions()
+                                .position(LatLng(lat, lng))
+                                .title(markerTitle) // ustawienie odpowiedniego tytułu markera
+                        )
                     }
-                },
+                }  catch (e: JSONException) {
+                    e.printStackTrace() // obsługa błędów parsowania JSON
+                }
+            },
                 Response.ErrorListener { error ->
                     // obsługa błędów związanych z żądaniem HTTP
                     Log.e("MapsActivity", "Błąd podczas wyszukiwania: ${error.message}")
@@ -294,16 +291,38 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
      */
     private fun placeMarkerOnMap(location: LatLng) {
         val markerOptions = MarkerOptions().position(location).title("Twoja lokalizacja")
-        mMap.addMarker(markerOptions) // dodaje marker dla bieżącej lokalizacji użytkownika
+        mMap.addMarker(markerOptions)   // dodaje marker dla bieżącej lokalizacji użytkownika
     }
+
     /**
      * Metoda wywoływana, gdy kliknięto na marker.
-     * Aktualnie nie wykonuje żadnej akcji.
      *
      * @param marker Marker, który został kliknięty.
-     * @return Zwraca false, aby nie zatrzymywać domyślnej akcji kliknięcia.
      */
-    override fun onMarkerClick(marker: Marker) = false
+    override fun onMarkerClick(marker: Marker): Boolean {
+        destinationLatLng = marker.position
+        Toast.makeText(this, "Wybrano miejsce: ${marker.title}. Kliknij 'Nawiguj' aby rozpocząć nawigację.", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    /**
+     * Metoda do nawigacji w mapach Google, po wybraniu odpowiedniego miejsca.
+     */
+    private fun openGoogleMapsForNavigation() {
+        if (destinationLatLng != null) {
+            val uri = "google.navigation:q=${destinationLatLng!!.latitude},${destinationLatLng!!.longitude}&mode=d"
+            val intent = Intent(Intent.ACTION_VIEW, android.net.Uri.parse(uri))
+            intent.setPackage("com.google.android.apps.maps")
+
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Zainstaluj aplikację Google Maps, aby korzystać z nawigacji.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Wybierz najpierw miejsce, klikając na pineskę.", Toast.LENGTH_SHORT).show()
+        }
+    }
     /**
      * Metoda do otwierania głównej aktywności.
      *
